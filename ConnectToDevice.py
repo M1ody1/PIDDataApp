d70cb0443e626e844d70c029f558b61354efe8c2
Kacper Wiśniewski <kacpererej@gmail.com>
2024-10-12 01:55:02 +0200

    Add commands and names of them

--- ConnectToDevice.py
+++ ConnectToDevice.py
@@ -3,3 +3,24 @@ import pyvisa
 rm = pyvisa.ResourceManager()
 print(f'Connected VISA resources: {rm.list_resources()}')
 
+value = -23.4 #for now example
+
+fieldnames = [
+    'Set Kp',
+    'Set Ki',
+    'Set Kd',
+    'Set Position',
+    'Emergency Stop',
+    'Allow motion',
+    'Go to Position 0'
+]
+
+commands = {
+    f'#k{value}%',
+    f'${value}%',
+    f'^{value}%',
+    f'*{value}%',
+    '!s%',
+    '@d#',
+    '-'
+}
